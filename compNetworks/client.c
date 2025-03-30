a989959f7b531b7230ec5f482511b30aa78a89c8
bleather <bleather@redcloud.uccs.edu>
2025-03-29 21:25:18 -0600

    Converted functions and Makefile to be Linux compatible. Ensured keys and certs are stored in the certs directory.

--- compNetworks/client.c
+++ compNetworks/client.c
@@ -1,38 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <winsock2.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <openssl/rand.h>
 #include <openssl/sha.h>
 #include <openssl/hmac.h>
 
-#pragma comment(lib, "ws2_32.lib") // Link with Winsock library
+#define SERVER_HOST "127.0.0.1"
+#define SERVER_PORT 8080
 
-#define SERVER_HOST "127.0.0.1"  // Server's IP address or hostname
-#define SERVER_PORT 8080         // Server's port
-
-// Function to handle errors
 void handle_errors(const char *msg) {
     fprintf(stderr, "%s\n", msg);
     ERR_print_errors_fp(stderr);
     exit(1);
 }
 
-// Function to initialize OpenSSL library
 void init_openssl() {
     SSL_load_error_strings();
     OpenSSL_add_ssl_algorithms();
 }
 
-// Function to cleanup OpenSSL resources
 void cleanup_openssl(SSL_CTX *ctx) {
     SSL_CTX_free(ctx);
     EVP_cleanup();
 }
 
-// HMAC function to ensure data integrity
 void hmac_sha256(const unsigned char *data, size_t data_len, const unsigned char *key, size_t key_len, unsigned char *out) {
     HMAC_CTX *ctx = HMAC_CTX_new();
     if (ctx == NULL) {
@@ -55,52 +51,38 @@ void hmac_sha256(const unsigned char *data, size_t data_len, const unsigned char
     HMAC_CTX_free(ctx);
 }
 
-// Main client function
 int main() {
-    WSADATA wsaData;
-    SOCKET sock;
+    int sock;
     struct sockaddr_in addr;
     SSL_CTX *ctx;
     SSL *ssl;
     char buffer[1024];
 
-    // Initialize Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        handle_errors("WSAStartup failed");
-    }
-
-    // Initialize OpenSSL
     init_openssl();
 
-    // Create a new SSL context
     ctx = SSL_CTX_new(TLS_client_method());
     if (!ctx) {
         handle_errors("Unable to create SSL context");
     }
 
-    // Load client certificate
     if (!SSL_CTX_use_certificate_file(ctx, "certs/client.crt", SSL_FILETYPE_PEM)) {
         handle_errors("Failed to load client certificate");
     }
 
-    // Load client private key
     if (!SSL_CTX_use_PrivateKey_file(ctx, "certs/client.key", SSL_FILETYPE_PEM)) {
         handle_errors("Failed to load client private key");
     }
 
-    // Verify client private key
     if (!SSL_CTX_check_private_key(ctx)) {
         handle_errors("Client private key does not match the certificate public key");
     }
 
-    // Load server certificate as CA certificate for validation
     if (!SSL_CTX_load_verify_locations(ctx, "certs/server.crt", NULL)) {
         handle_errors("Failed to load CA certificate");
     }
 
-    // Create a socket
-    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (sock == INVALID_SOCKET) {
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
         handle_errors("Unable to create socket");
     }
 
@@ -108,12 +90,10 @@ int main() {
     addr.sin_port = htons(SERVER_PORT);
     addr.sin_addr.s_addr = inet_addr(SERVER_HOST);
 
-    // Connect to the server
-    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == SOCKET_ERROR) {
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         handle_errors("Unable to connect to server");
     }
 
-    // Create SSL structure and connect it to the socket
     ssl = SSL_new(ctx);
     if (!ssl) {
         handle_errors("Unable to create SSL structure");
@@ -121,47 +101,41 @@ int main() {
 
     SSL_set_fd(ssl, sock);
 
-    // Perform the SSL handshake
     if (SSL_connect(ssl) <= 0) {
         handle_errors("SSL handshake failed");
     }
 
-    // Print the connection details
     printf("Connected with %s encryption\n", SSL_get_cipher(ssl));
 
-    // Send a message to the server
     const char *message = "Hello, Server!";
     if (SSL_write(ssl, message, strlen(message)) <= 0) {
         handle_errors("Failed to send message");
     }
     printf("Message sent to server: %s\n", message);
 
-    // Create HMAC for data integrity
     unsigned char hmac_result[SHA256_DIGEST_LENGTH];
-    unsigned char secret_key[] = "my_secret_key";  // Use a fixed secret key for HMAC
+    unsigned char secret_key[] = "my_secret_key";
     hmac_sha256((unsigned char *)message, strlen(message), secret_key, sizeof(secret_key) - 1, hmac_result);
 
-    // Send HMAC to the server
     if (SSL_write(ssl, hmac_result, SHA256_DIGEST_LENGTH) <= 0) {
         handle_errors("Failed to send HMAC");
     }
     printf("HMAC sent to server\n");
 
-    // Read the server's response
     int bytes_received = SSL_read(ssl, buffer, sizeof(buffer) - 1);
-if (bytes_received <= 0) {
-    perror("Failed to read response from server");
-    exit(1);
-}
+    if (bytes_received <= 0) {
+        perror("Failed to read response from server");
+        exit(1);
+    }
 
-buffer[bytes_received] = '\0'; // Null-terminate the response
-printf("Server response: %s\n", buffer);
+    buffer[bytes_received] = '\0';
+    printf("Server response: %s\n", buffer);
 
-    // Cleanup
+    printf("Closing connection to server\n");
     SSL_shutdown(ssl);
-    closesocket(sock);
+    close(sock);
     cleanup_openssl(ctx);
-    WSACleanup();
 
     return 0;
 }
+
--- compNetworks/server.c
+++ compNetworks/server.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>        // For close()
+#include <arpa/inet.h>     // For inet_addr() and socket functions
+#include <sys/socket.h>    // For socket functions
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <openssl/hmac.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
 
 #define PORT 8080
 #define SECRET_KEY "mysecretkey" // Pre-shared key for HMAC
@@ -43,8 +44,8 @@ void communicate(SSL *ssl) {
     char buffer[1024];
     int bytes_received = SSL_read(ssl, buffer, sizeof(buffer) - 1);
     if (bytes_received <= 0) {
-    perror("Failed to read message from client");
-    exit(1);
+        perror("Failed to read message from client");
+        exit(1);
     }
 
     buffer[bytes_received] = '\0'; // Null-terminate received data
@@ -60,12 +61,11 @@ void communicate(SSL *ssl) {
     SSL_write(ssl, hmac_result, hmac_len);
 
     printf("Sent message with HMAC verification\n");
+
+    sleep(1);
 }
 
 int main() {
-    WSADATA wsa;
-    WSAStartup(MAKEWORD(2, 2), &wsa);
-
     SSL_load_error_strings();
     OpenSSL_add_ssl_algorithms();
 
@@ -73,10 +73,10 @@ int main() {
     if (!ctx) handle_errors("Failed to create SSL context");
 
     if (!SSL_CTX_use_certificate_file(ctx, "certs/server.crt", SSL_FILETYPE_PEM))
-    handle_errors("Failed to load server certificate");
+        handle_errors("Failed to load server certificate");
 
     if (!SSL_CTX_use_PrivateKey_file(ctx, "certs/server.key", SSL_FILETYPE_PEM))
-    handle_errors("Failed to load server private key");
+        handle_errors("Failed to load server private key");
 
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) handle_errors("Socket creation failed");
@@ -94,7 +94,7 @@ int main() {
     printf("Server listening on port %d...\n", PORT);
 
     struct sockaddr_in client_addr;
-    int client_len = sizeof(client_addr);
+    socklen_t client_len = sizeof(client_addr);  // Use `socklen_t` for Linux
     int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
     if (client_fd == -1) handle_errors("Accept failed");
 
@@ -111,8 +111,9 @@ int main() {
     // Cleanup
     SSL_shutdown(ssl);
     SSL_free(ssl);
-    closesocket(client_fd);
-    closesocket(server_fd);
+    close(client_fd);
+    close(server_fd);
     SSL_CTX_free(ctx);
-    WSACleanup();
+    printf("Connection closed\n");
 }
+
